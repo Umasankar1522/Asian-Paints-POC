fc4708d508a1c00d9f91792b6532b4abcb3441c9
umasankar1522 <umasankar.k@emproto.com>
2023-01-20 11:59:35 +0530

    add: Stencils logic

--- src/Components/ImageUpload.component.jsx
+++ src/Components/ImageUpload.component.jsx
@@ -1,12 +1,58 @@
 import React, {useEffect, useRef, useState} from 'react';
 import styles from './ImageUpload.module.css';
+import stencil from '../../src/stencil.png'
+import stencil2 from '../../src/stencil2.png';
 
 const ImageUpload = () => {
 
     const canvasRef = useRef();
     const [selectedImage, setSelectedImage] = useState('');
     const [selectedColor, setSelectedColor] = useState('#000000');
+    const [stencilSelected, setStencilSelected] = useState('');
+    const [stencilImageMoveId, setStencilImageMoveId] = useState('');
 
+    useEffect(() => {
+        const canvas = canvasRef?.current;
+        const listener = (event) => {
+            const { x, y} = getCursorPosition(canvas, event);
+            const createdStencil = document.createElement('img');
+            createdStencil.src = stencilSelected;
+            createdStencil.width = 40;
+            createdStencil.style.position = 'absolute';
+            createdStencil.setAttribute('draggable', 'true');
+            createdStencil.style.top = y.toString() + 'px';
+            createdStencil.style.left = x.toString() + 'px';
+            createdStencil.id = new Date().getTime();
+            createdStencil.ondragstart = (event) => {
+                setStencilImageMoveId(event.target.id);
+            }
+
+            document.getElementById('canvasStencil').appendChild(createdStencil);
+            setStencilSelected('');
+        }
+        
+        if(stencilSelected ) {
+            canvas.addEventListener('click', listener);
+        }
+
+        return () => {
+            canvas.removeEventListener('click', listener);
+        }
+    }, [stencilSelected]);
+
+
+
+
+    const onImageSelected = (event) => {
+        if(selectedImage){
+            if(stencilSelected) {
+                setStencilSelected('');
+            } else {
+                setStencilSelected(event.target.src);
+            }
+        }
+    };
+ 
     // Image Upload Handler
     const onImageUpload = (event) => {
         const file = event.target.files[0];
@@ -18,7 +64,7 @@ const ImageUpload = () => {
             reader.readAsDataURL(file);
           } else {
             setSelectedImage('');
-          }
+        }
     };
 
     // Color Change Handler
@@ -50,13 +96,14 @@ const ImageUpload = () => {
     // onClick of mouse --> get Current Coordinates of X and Y with respect to the Image
     function getCursorPosition(canvas, event) {
         const rect = canvas.getBoundingClientRect();
-        const x = event.clientX - rect.left
-        const y = event.clientY - rect.top
-      return {x, y};
+        const x = event.clientX - rect.x
+        const y = event.clientY - rect.y
+        const {top ,left, right, bottom} = rect;
+      return {x, y, top, left, right, bottom};
     }
 
     useEffect(() => {
-        if(selectedImage !== '')
+        if(selectedImage !== '' && stencilSelected === '')
             drawImage(selectedImage);
         else
             return;
@@ -69,13 +116,19 @@ const ImageUpload = () => {
             console.log(coordinates);
         };
 
-        canvas.addEventListener('mouseup', listener);
+        if(!stencilSelected)
+            canvas.addEventListener('mouseup', listener);
 
         return () => {
             canvas.removeEventListener('mouseup', listener);
         }
-    }, [selectedColor, selectedImage]);
+    }, [selectedColor, selectedImage, stencilSelected]);
 
+    const onDropOfStencil = (event) => {
+        const {x, y} = getCursorPosition(canvasRef?.current, event);
+        document.getElementById(stencilImageMoveId).style.top = y.toString() + 'px';
+        document.getElementById(stencilImageMoveId).style.left = x.toString() + 'px';
+    }
 
     return (
         <div className={styles.imageUploadContainer}>
@@ -93,8 +146,14 @@ const ImageUpload = () => {
                 <label htmlFor='colorPicker' />
                 <input type='color' name='color picker' id='colorPicker' value={selectedColor} onChange={onColorChange}/>
             </div>
+            <div className={styles.stencilContainer}>
+                <img src={stencil} alt="stencil" style={{border: `${stencilSelected.includes(stencil) ? '1px solid black': '0'}` }} onClick={onImageSelected} />
+                <img src={stencil2} alt="stencil" style={{border: `${stencilSelected.includes(stencil2) ? '1px solid black': '0'}` }} onClick={onImageSelected} />
+
+            </div>
             <div className={styles.mainCanvasArea}>
-                <div className={styles.canvasContainer} >
+                <div className={styles.canvasContainer} id={'canvasStencil'} style={{position: 'relative'}} 
+                onDragOver={(event) => {event.preventDefault()}} onDrop={onDropOfStencil}>
                     <canvas id='image-canvas' ref={canvasRef} />
                 </div>
             </div>
